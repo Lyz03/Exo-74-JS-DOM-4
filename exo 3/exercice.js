c94e5102357f440592fcc461071f59d8c22fa8dd
Lyz03 <lizoe@netcourrier.com>
2021-11-18 10:36:34 +0100

    part 3

--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,10 +1,11 @@
 let libButton = document.getElementById('lib-button');
 let libIt = function() {
     let storyDiv = document.getElementById("story");
-    let noun = document.getElementById('noun');
-    let adjective = document.getElementById('adjective');
-    let name = document.getElementById('person');
-    storyDiv.innerHTML = "??Your story here";
+    let noun = document.getElementById('noun').value;
+    let adjective = document.getElementById('adjective').value;
+    let name = document.getElementById('person').value;
+
+    storyDiv.innerHTML = "Your story here with : " +name+ " who is " +adjective+ " and love " +noun;
 };
 libButton.addEventListener('click', libIt);
 
