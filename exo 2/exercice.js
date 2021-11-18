a347fb2efeb3490f5498375cf1bc17f96cf6955b
Lyz03 <lizoe@netcourrier.com>
2021-11-18 10:27:52 +0100

    part 2

--- exo 2/exercice.js
+++ exo 2/exercice.js
@@ -1,8 +1,12 @@
 let catPic = document.getElementById("cat-pic");
+
 let onCatClick = function(e) {
-    var stashePic = document.getElementById("mustache-pic");
-    stashePic.style.top = "100px";
-    stashePic.style.left = "100px";
+    let stashePic = document.getElementById("mustache-pic");
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+    console.log(mouseX);
+    stashePic.style.top = mouseY.toString()+ 'px';
+    stashePic.style.left = (mouseX - 40).toString()+ 'px';
 };
 
 catPic.addEventListener("click", onCatClick);
--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,6 +1,9 @@
 let libButton = document.getElementById('lib-button');
 let libIt = function() {
     let storyDiv = document.getElementById("story");
+    let noun = document.getElementById('noun');
+    let adjective = document.getElementById('adjective');
+    let name = document.getElementById('person');
     storyDiv.innerHTML = "??Your story here";
 };
 libButton.addEventListener('click', libIt);
