edd9ea5201e99a2073a88fc5ac6d6c33bff0129d
Lyz03 <lizoe@netcourrier.com>
2021-11-18 10:04:50 +0100

    part 1

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,7 +1,12 @@
 //Votre code ci dessous
 
+let cat = document.getElementById('cat-pic');
 
+function miaou() {
+    document.getElementById('cat-chat').innerText = 'Miaou'
+}
 
+cat.addEventListener("click", miaou);
 
 
 
